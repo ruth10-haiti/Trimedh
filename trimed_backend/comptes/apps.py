c347524f80a0c4453e4e06c9178b21da2cbfc1de
ruth10-haiti <ruthdieuveuille09@gmail.com>
2026-02-14 15:06:32 -0500

    l'user

--- trimed_backend/comptes/apps.py
+++ trimed_backend/comptes/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
 
-
 class ComptesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comptes'
     verbose_name = 'Gestion des Comptes'
-    
+
     def ready(self):
         import comptes.signals
+        from .init_admin import create_admin
+        create_admin()
