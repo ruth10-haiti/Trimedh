8af11a78ab0754f8f76c86a096b3d2b1e27236de
ruth10-haiti <ruthdieuveuille09@gmail.com>
2026-03-20 12:32:30 -0500

    Fix: Correction complete des imports et ViewSets

--- patients/urls.py
+++ patients/urls.py
@@ -1,19 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    PatientViewSet, AdressePatientViewSet, PersonneAContacterViewSet,
-    AssurancePatientViewSet, AllergiePatientViewSet, AntecedentMedicalViewSet,
+    PatientViewSet,
+    AdressePatientViewSet,
+    PersonneAContacterViewSet,
+    AssurancePatientViewSet,
+    AllergiePatientViewSet,
+    AntecedentMedicalViewSet,
     SuiviPatientViewSet
 )
 
 router = DefaultRouter()
-router.register(r'', PatientViewSet, basename='patient')
-router.register(r'adresses', AdressePatientViewSet, basename='adresse-patient')
-router.register(r'contacts', PersonneAContacterViewSet, basename='contact-patient')
-router.register(r'assurances', AssurancePatientViewSet, basename='assurance-patient')
-router.register(r'allergies', AllergiePatientViewSet, basename='allergie-patient')
-router.register(r'antecedents', AntecedentMedicalViewSet, basename='antecedent-patient')
-router.register(r'suivis', SuiviPatientViewSet, basename='suivi-patient')
+router.register(r'patients', PatientViewSet)
+router.register(r'adresses', AdressePatientViewSet)
+router.register(r'contacts', PersonneAContacterViewSet)
+router.register(r'assurances', AssurancePatientViewSet)
+router.register(r'allergies', AllergiePatientViewSet)
+router.register(r'antecedents', AntecedentMedicalViewSet)
+router.register(r'suivis', SuiviPatientViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
